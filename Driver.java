d0a6363c6f3b1a5e703cb3689ba23734443a5abf
Josh Ullman <ullmanjosh@yahoo.com>
2019-10-10 22:19:47 -0500

    19

--- Driver.java
+++ Driver.java
@@ -8,7 +8,7 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException 
 	{
-		String stID = "IDAB";
+		String stID = "HOLD";
 		MesoInherit mesoInherit = new MesoInherit(new MesoStation(stID));
 		
 		PosAvg posAvg = new PosAvg(stID);
